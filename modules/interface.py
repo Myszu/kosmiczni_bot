9c377abb7aecb60ee8e5d0aae9925386692dd1d0
Myszu <nowacki0508@gmail.com>
2026-03-17 23:28:06 +0100

    Remodeling of building interfaces functions.

--- modules/interface.py
+++ modules/interface.py
@@ -1,3 +1,5 @@
+from typing import Literal
+from pydantic import BaseModel
 from selenium.webdriver.firefox import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
@@ -13,8 +15,22 @@ class UserInterface():
         
     def prepare_top_menu(self) -> None:
         top_bar = self.browser.find_element(By.ID, 'top_menu')
-        self.map: WebElement = top_bar.find_element(By.ID, 'map_link_btn')
         sections = top_bar.find_elements(By.TAG_NAME, 'button')
+        for section in sections:
+            if section.get_attribute('data-page') == 'game_klan':
+                clan: WebElement = section
+            elif section.get_attribute('data-page') == 'game_raps':
+                reports: WebElement = section
+            elif section.get_attribute('data-page') == 'game_pw':
+                messages: WebElement = section
+        if not all([top_bar, clan, reports, messages]):
+            return
+        self.buttons = Buttons(
+            world_map=top_bar.find_element(By.ID, 'map_link_btn'),
+            clan=clan,
+            reports=reports,
+            messages=messages
+        )
     
     def prepare_quick_bar(self) -> None:
         quick_bar = self.browser.find_element(By.ID, 'quick_bar')
@@ -32,4 +48,23 @@ class UserInterface():
                 self.potions: WebElement = option
             elif option.get_attribute('data-option') == 'game_empire':
                 self.empire: WebElement = option
-            
+    
+    def goto(self, element: Literal["Teleport", "Daily", "Blessings", "Potions", "Empire"]):
+        if element == "Teleport":
+            self.teleport.click()
+        elif element == "Daily":
+            self.daily.click()
+        elif element == "Blessings":
+            self.blessings.click()
+        elif element == 'Potions':
+            self.potions.click()
+        elif element == 'Empire':
+            self.empire.click()
+        else:
+            l
+    
+class Buttons(BaseModel):
+    world_map: WebElement | None
+    clan: WebElement | None
+    reports: WebElement | None
+    messages: WebElement | None
